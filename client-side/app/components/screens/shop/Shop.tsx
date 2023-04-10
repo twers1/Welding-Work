0082bdabd9ff7f38c2342b66ae615ff860215073
Sima <tarverdans15@gmail.com>
2023-04-10 16:00:12 +0300

    edit shop and globals.scss

--- client-side/app/components/screens/shop/Shop.tsx
+++ client-side/app/components/screens/shop/Shop.tsx
@@ -14,11 +14,13 @@ const Shop: FC<IProductsPage> = ({products}) => {
 			<Row className='bg-primary'>
                 {products.map((product, index)   => (
                    <Colum size={index === 0 ? 3 : 3} key={product.id}>
-                    <div className='text-gray'>
+                    <div className='text-gray text-center'>
+                        <div className='mb-2 '>
                         <Image alt={product.name} src={product.images[0]} width={220} height={220} />
-                        <h3>{product.name}</h3>
+                        <h2 className='font-serif'>{product.name}</h2>
                         <div className=''>ОТ {product.price} РУБ</div>
                         <button>Добавить в корзину</button>
+                        </div>
                     </div>
                    </Colum> 
                 ))}
